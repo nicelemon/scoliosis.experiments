9e65dc81885466ce4fef728589b2753f2006a920
weirdmojo <zackry.lerner@gmail.com>
2014-01-14 23:09:31 -0500

    Lots more stuff
    *messing with shapes
    *getting stuff to move

--- jambajuice/my-main.js
+++ jambajuice/my-main.js
@@ -1,6 +1,6 @@
 $(function() {
-    $(".rando_stripe").each(function() {
-        var hue = 'rgb(' + (Math.floor((256-199)*Math.random()) + 200) + ',' + (Math.floor((256-199)*Math.random()) + 200) + ',' + (Math.floor((256-199)*Math.random()) + 200) + ')';
+    $(".rando").each(function() {
+        var hue = 'rgb(' + (Math.floor((256-199)*Math.random() + 200*Math.random())) + ',' + (Math.floor((256-199)*Math.random()) + 50) + ',' + (Math.floor((256-199)*Math.random()) + 50) + ')';
          $(this).css("background-color", hue);
     });
 });
